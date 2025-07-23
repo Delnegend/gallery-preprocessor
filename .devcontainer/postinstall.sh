46705529aed7fb8509e6c143a6b722da0caf0fd0
Delnegend <kiennguyen19323@gmail.com>
2025-07-23 08:27:17 +0700

    build(`.devcontainer`): add just, prettier, pretty ts error

--- .devcontainer/postinstall.sh
+++ .devcontainer/postinstall.sh
@@ -11,6 +11,22 @@ echo 'export PATH="$VOLTA_HOME/bin:$PATH"' >> ~/.zshrc
 volta install node@lts pnpm && pnpm config set store-dir ~/.pnpm-store
 cd frontend && pnpm install && cd ..
 
+# just
+ver="1.42.3"
+curl -L -o /tmp/just.tar.gz https://github.com/casey/just/releases/download/$ver/just-$ver-x86_64-unknown-linux-musl.tar.gz
+checksum=$(openssl dgst -sha3-512 /tmp/just.tar.gz | awk '{print $2}')
+expected="939eef7b9105e4805825b6cf7aac7bb2e8daf1aa07e4e8cfce619e1e447a444847318abd0c7e0ac3553e80a98e8d465158c7574e1fc7d2232e839b021f9dad67"
+if [ ! "$checksum" = "$expected" ]; then
+    echo "just tarball checksum failed\nexpected: $expected\ngot: $checksum"
+else
+    sudo rm -rf /usr/local/bin/just
+    sudo tar -xf /tmp/just.tar.gz -C /usr/local/bin just
+fi
+rm -f /tmp/just.tar.gz
+echo 'alias j=just' >> ~/.zshrc
+just --completions zsh > ~/.just.zsh
+echo '[[ -f ~/.just.zsh ]] && source ~/.just.zsh' >> ~/.zshrc
+
 # go
 if [ -f /usr/local/go/bin/go ]; then
     echo "Go is already installed."
@@ -21,19 +37,16 @@ else
     rm go.tar.gz
 fi
 
-# add /usr/local/go/bin to path
+# PATH stuffs
 if [[ ":$PATH:" != *":/usr/local/go/bin:"* ]]; then
     echo "Adding /usr/local/go/bin to PATH..."
     echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc
     source ~/.zshrc
 fi
-
-# add ~/go/bin to path
 if [[ ":$PATH:" != *":~/go/bin:"* ]]; then
     echo "Adding ~/go/bin to PATH..."
     echo 'export PATH=$PATH:~/go/bin' >> ~/.zshrc
     source ~/.zshrc
 fi
 
-# wails
 go install github.com/wailsapp/wails/v2/cmd/wails@latest
